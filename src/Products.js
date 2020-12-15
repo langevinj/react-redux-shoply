0c6f63081731b7863310a1d50acd502f0e19b327
Jeremy Langevin <langevinj832@gmail.com>
2020-12-15 12:11:57 -0500

    styling

--- src/Products.js
+++ src/Products.js
@@ -7,11 +7,22 @@ function Products() {
     const products = useSelector(st => st.inventory)
 
     return (
-        <ul>
-            {products ? Object.entries(products).map(product => <li key={product[0]}><ProductCard id={product[0]} item={product[1]} /></li>) : null}
-        </ul>
+        <div className="container-fluid">
+            <div className="row bg-white">
+                <div className="col"></div>
+                <div className="col"><h2>The best deals guarantee!</h2></div>
+                <div className="col"></div>
+            </div>
+            <div className="row bg-light">
+                <div className="col"></div>
+                <div className="col">
+                <ul>
+                    {products ? Object.entries(products).map(product => <li key={product[0]}><ProductCard id={product[0]} item={product[1]} /></li>) : null}
+                </ul></div>
+                <div className="col"></div>
+            </div>
+        </div>
     )
-
 }
 
 export default Products;

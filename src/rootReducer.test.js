207f007a964a55e0de03f7cda1090ae182fff440
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 11:13:46 -0500

    Rewrote inventory checking tests for rootReducer.js tests

--- src/rootReducer.test.js
+++ src/rootReducer.test.js
@@ -10,14 +10,14 @@ beforeEach(() => {
 
 it("should return the correct initial values", function() {
     expect(store.getState().cart).toEqual({})
-    expect(store.getState().inventory).toEqual(expect.any(Object))
+    expect(Object.keys(store.getState().inventory)).toHaveLength(6)
 });
 
 it("should return the correct cart when an item is added", function() {
     let action = { type: 'ADD', payload: "47314fa1-ae56-4eae-80be-af6691145951" }
     store.dispatch(action);
     expect(Object.keys(store.getState().cart)).toHaveLength(1)
-    expect(store.getState().inventory).toEqual(expect.any(Object))
+    expect(Object.keys(store.getState().inventory)).toHaveLength(6)
 });
 
 it("should remove an item from the cart when removed", function() {

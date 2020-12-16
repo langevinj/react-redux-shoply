c865a8c8cfb4151f8385f55c55e046174ffe1d9a
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 11:19:54 -0500

    Smoke and Snapshot tests written for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider }  from 'react-redux'
+import rootReducer from './rootReducer'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+let store;
+
+beforeEach(() => {
+  store = createStore(rootReducer);
+})
+
+it("renders without crashing", function() {
+  render(
+    <Provider store={store}>
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+    </Provider>)
+})
+
+it("matches the snapshot", function() {
+  const { asFragment } = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>)
+
+    expect(asFragment).toMatchSnapshot();
+})

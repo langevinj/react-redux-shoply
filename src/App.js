0c9adaf9fdc1f804d38a15017a57dce00bd7314e
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 09:07:13 -0500

    Clean up and documentation of App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import Routes from './Routes'
-import Nav from './Nav'
 import useLocalStorage from './hooks'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import { updateInitialCart } from './actions'
+import Routes from './Routes'
+import Nav from './Nav'
+import './App.css';
 
 function App() {
-  const [storedCart, setStoredCart] = useLocalStorage("cart")
+  const [storedCart] = useLocalStorage("cart")
   const dispatch = useDispatch();
 
+  //on rendering, use the locally stored cart to set the store
   useEffect(() => {
     dispatch(updateInitialCart(storedCart))
   }, [])
   
-  // let msg = storedCart === [] ? JSON.parse(storedCart) : null
-  console.log(storedCart)
   return (
     <div className="App">
       <Nav />

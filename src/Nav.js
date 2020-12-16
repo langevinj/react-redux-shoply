525e3cdcdbf69f84f4f81f2ac4b0a36ce168eaa1
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 09:11:34 -0500

    Clean up and documentation of Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { calculateTotal, totalItems } from './helpers'
-import './Nav.css'
 import useLocalStorage from './hooks'
+import './Nav.css'
 
+//renders the navbar
 function Nav() {
     const cart = useSelector(st => st.cart)
     const [storedCart, setStoredCart] = useLocalStorage("cart")
 
+    //whenever the cart is updated by redux, also update the locally stored cart
     useEffect(() => {
         function updateLocalCart(){
             setStoredCart(storedCart => cart)
@@ -23,10 +25,8 @@ function Nav() {
                 </NavLink>
             
             <NavLink exact to="/cart">
-                {/* <span>Cart  {totalItems(cart)}</span> */}
                 <i className="fas fa-shopping-cart fa-2x"><span className="item-count">{totalItems(cart)}</span>
                 <span className="cart-total rounded p-1">{calculateTotal(cart).total}</span></i>
-                
             </NavLink>
         </nav>
     )

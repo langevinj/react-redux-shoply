95ddec827719b860f8ba106c93fcd20c1ad54909
Jeremy Langevin <langevinj832@gmail.com>
2020-12-16 09:09:48 -0500

    Clean up and documentation of Details component

--- src/Details.js
+++ src/Details.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import ProductCard from './ProductCard'
 
 function Details() {
+    //get id of product from query string
     const { id } = useParams();
     const products = useSelector(st => st.inventory)
 
